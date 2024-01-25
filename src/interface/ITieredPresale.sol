46ea291e63307ab1ecdcb0257e4cbf827af3c91f
Semi Invader <=>
2024-01-25 14:43:34 -0600

    saleStatus view, rewards to claim gives more details, change Layer struct, add referral to user layer

--- src/interface/ITieredPresale.sol
+++ src/interface/ITieredPresale.sol
@@ -2,14 +2,23 @@
 pragma solidity 0.8.23;
 
 interface ITieredPresale {
+    /// @dev to change from PENDING to IN_PROGRESS, status has to check if startBlock is reached and currentLayerId == 0
+    enum Status {
+        PENDING, // sale is not started yet
+        IN_PROGRESS, // sale is in progress
+        COMPLETED, // sale is completed
+        CANCELLED // sale is cancelled
+    }
     struct LayerInfo {
-        address[] usersOnGrid;
+        address[] usersOnGrid; // users participating
         uint256 tokensSold;
         uint256 pricePerGrid;
+        uint256 startBlock;
+        uint256 endBlock;
         uint8 liquidityBasisPoints;
         uint8 referralBasisPoints;
         uint8 previousLayerBasisPoints;
-        uint8 grids;
+        uint8 gridsOccupied; // number of grids occupied in this layer
         uint8 prevLayerId;
     }
 
@@ -17,13 +26,42 @@ interface ITieredPresale {
         uint8[] layerPositions;
         uint256 totalDeposit;
         uint256 totalTokensToClaim;
+        address referral;
     }
 
-    function deposit(uint8 gridId, uint amount) external;
+    function deposit(uint8 gridId, uint amount, address referral) external;
 
     function refund(uint8 gridId) external;
 
-    function claimTokens() external;
+    function claimTokensAndRewards() external;
+
+    // ----------------------------------
+    // OWNER FUNCTIONS
+    // ----------------------------------
+    function setLayerStartBlock(uint8 layerId, uint256 startBlock) external;
+
+    function setLayerDuration(uint8 layerId, uint256 duration) external;
+
+    function setLayerPricePerGrid(uint8 layerId, uint256 pricePerGrid) external;
+
+    function setLayerLiquidityBasisPoints(
+        uint8 layerId,
+        uint8 liquidityBasisPoints
+    ) external;
+
+    function setLayerReferralBasisPoints(
+        uint8 layerId,
+        uint8 referralBasisPoints
+    ) external;
+
+    function setLayerPreviousLayerBasisPoints(
+        uint8 layerId,
+        uint8 previousLayerBasisPoints
+    ) external;
+
+    //----------------------------------
+    // VIEW FUNCTIONS
+    //----------------------------------
 
     function currentLayerId() external view returns (uint8);
 
@@ -38,6 +76,16 @@ interface ITieredPresale {
     function saleToken() external view returns (address);
 
     function receiveToken() external view returns (address);
+
+    function saleStatus() external view returns (Status);
+
+    function rewardsToClaim(
+        uint8 layerId,
+        address user
+    )
+        external
+        view
+        returns (uint256 allTokens, uint referral, uint referralTokens);
 
     /*---------------------------------------
      *  EVENTS
