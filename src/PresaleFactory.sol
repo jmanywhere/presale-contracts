ba08dfa37b09b52e06bec564811a1b0092b8d43a
Semi Invader <=>
2024-01-31 16:45:44 -0600

    presale factory v1

--- src/PresaleFactory.sol
+++ src/PresaleFactory.sol
@@ -74,15 +74,87 @@ contract Factory is ITieredPresaleFactory, Ownable {
         platformSellFee = _newFee;
     }
 
+    function _safeTokenTransfer(
+        address token,
+        address to,
+        uint256 amount
+    ) private {
+        (bool succ, ) = token.call(
+            abi.encodeWithSelector(IERC20.transfer.selector, to, amount)
+        );
+        if (!succ) revert TPresale__CouldNotTransfer(token, amount);
+    }
+
+    function claimTokens(address _token, address _to) external onlyOwner {
+        if (_token == address(0)) {
+            (bool succ, ) = _to.call{value: address(this).balance}("");
+            require(succ, "Failed to send Ether");
+            return;
+        }
+        _safeTokenTransfer(
+            _token,
+            _to,
+            IERC20(_token).balanceOf(address(this))
+        );
+    }
+
     //-------------------------------------------------------------------
     // VIEW FUNCTIONS
     //-------------------------------------------------------------------
     // ONLY FOR UI and to not to be used in smart contracts
     //-------------------------------------------------------------------
     //-------------------------------------------------------------------
-    function getInProgress() external view returns (address[] memory) {}
+    function getInProgress() external view returns (address[] memory) {
+        uint allPresales = presales.length;
+        uint inProgress = 0;
+        uint ipOffset = 0;
+        for (uint i = 0; i < allPresales; i++) {
+            if (
+                TieredPresale(presales[i]).status() ==
+                ITieredPresale.Status.IN_PROGRESS
+            ) {
+                inProgress++;
+            }
+        }
+        address[] memory inProgressPresales = new address[](inProgress);
+        for (uint i = 0; i < allPresales; i++) {
+            if (
+                TieredPresale(presales[i]).status() ==
+                ITieredPresale.Status.IN_PROGRESS
+            ) {
+                inProgressPresales[ipOffset] = presales[i];
+                ipOffset++;
+            }
+        }
+        return inProgressPresales;
+    }
 
-    function getCompleted() external view returns (address[] memory) {}
+    function getCompleted() external view returns (address[] memory) {
+        uint allPresales = presales.length;
+        uint completed = 0;
+        uint offset = 0;
+        for (uint i = 0; i < allPresales; i++) {
+            if (
+                TieredPresale(presales[i]).status() ==
+                ITieredPresale.Status.FINALIZED
+            ) {
+                completed++;
+            }
+        }
+        address[] memory completedPresales = new address[](completed);
+        for (uint i = 0; i < allPresales; i++) {
+            if (
+                TieredPresale(presales[i]).status() ==
+                ITieredPresale.Status.FINALIZED
+            ) {
+                completedPresales[offset] = presales[i];
+                offset++;
+            }
+        }
+        return completedPresales;
+    }
 
-    function getAll() external view returns (address[] memory) {}
+    function getAll() external view returns (address[] memory) {
+        return presales;
+    }
 }
